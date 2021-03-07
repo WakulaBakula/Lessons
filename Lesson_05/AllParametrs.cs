3093f0a703c62a3effc77c42ff62c466a7bd8a53
Ivan Korneev <www.unreal.90@bk.ru>
2021-03-07 17:11:44 +0300

    New classes
    Каждое задание выведено в отдельный клас, добавлена сериализация Bin

---
+++ Lesson_05/AllParametrs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_05
+{
+    [Serializable]
+    public class AllParametrs
+    {
+
+        public string Filename { get; }
+
+
+        public byte MasBin { get; }
+
+        public int N { get; }
+
+       
+
+
+      
+    }
+}
