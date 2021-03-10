577b6f5d292e6472f5043389099e0d94c32bedae
Ivan Korneev <www.unreal.90@bk.ru>
2021-03-10 18:56:40 +0300

    DZ
    1 задание

---
+++ Lesson_06/Lesson_06/.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FullLessons.Lesson_06
+{
+    class Class1
+    {
+        public static void ReadTxt()
+        {
+
+            string Path = @"C: \Users\korneev.i\Documents\Temp\ReadTxt";
+            StreamReader textstream = new StreamReader(@"{Path}\list.txt", System.Text.Encoding.Default);
+            ArrayList arr = new ArrayList();
+            Regex regex = new Regex(@"\d{2}-\d{6}-\d{5}");
+
+            foreach (var item in textstream)
+            {
+                using (FileStream fileStream = new FileStream($"{Path}\list.txt", FileMode.OpenOrCreate))
+                {
+
+                }
+            }
+            //string lines = File.ReadAllText(@"C:\Users\korneev.i\Documents\Temp\ReadTxt\list.txt");
+            
+            //int count = 0;
+            //for (int i = 0; i < lines.Length; i++)
+            //{
+            //    File.WriteAllText(@"C:\Users\korneev.i\Documents\Temp\ReadTxt\Result.txt", )
+            //}
+
+
+
+
+        }
+        
+    }
+}
---
+++ Lesson_06/Lesson_06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace FullLessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Задание 1 Вывод содержимого каталога в текстовый файл
+            Console.WriteLine("Задание 1 Вывод содержимого каталога в текстовый файл");
+            string WorkDir = @"C:\Dz";
+            string filename = @"C:\Dz\text.txt";
+            Console.WriteLine(Directory.Exists(WorkDir));
+            string notesDir = Path.Combine(WorkDir, "Notes");
+            Directory.CreateDirectory(notesDir);
+            string[] entries = Directory.GetFileSystemEntries(WorkDir, "*", SearchOption.AllDirectories);
+
+            for (int i = 0; i < entries.Length; i++)
+            {
+                Console.WriteLine(entries[i]);
+                
+            }
+            File.WriteAllLines(filename, entries);
+            
+        }
+    }
+}
